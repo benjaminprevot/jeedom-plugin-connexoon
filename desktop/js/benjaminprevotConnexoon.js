0a94890f31679d5ca36afbfe151917f9ef192d65
Benjamin PREVOT <me@benjaminprevot.fr>
2019-12-27 16:27:55 +0100

    Remove unnecessary action button

--- desktop/js/benjaminprevotConnexoon.js
+++ desktop/js/benjaminprevotConnexoon.js
@@ -34,7 +34,6 @@ function addCmdToTable(_cmd) {
     tr += '</td>';
     tr += '<td>';
     if (is_numeric(_cmd.id)) {
-        tr += '<a class="btn btn-default btn-xs cmdAction" data-action="configure"><i class="fa fa-cogs"></i></a> ';
         tr += '<a class="btn btn-default btn-xs cmdAction" data-action="test"><i class="fa fa-rss"></i> {{Tester}}</a>';
     }
     tr += '</td>';
