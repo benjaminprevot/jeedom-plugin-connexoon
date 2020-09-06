e27c3ef0a64249b14f48dc5dc187069a6d7b9e4b
Benjamin PREVOT <me@benjaminprevot.fr>
2020-09-06 14:20:04 +0200

    [CHANGED] Use utility classes

--- desktop/modal/authorization.php
+++ desktop/modal/authorization.php
@@ -3,21 +3,21 @@ if (!isConnect('admin')) {
     throw new Exception('{{401 - Accès non autorisé}}');
 }
 
-benjaminprevotConnexoon::removeConfig('access_token');
-benjaminprevotConnexoon::removeConfig('refresh_token');
-benjaminprevotConnexoon::removeConfig('token_exists');
+Config::unset('access_token');
+Config::unset('refresh_token');
+Config::unset('token_exists');
 
 $state = hash("sha256", rand());
-benjaminprevotConnexoon::setConfig('consumer_state', $state);
+Config::set('consumer_state', $state);
 
-$consumer_key = benjaminprevotConnexoon::getConfig('consumer_key');
+$consumer_key = Config::get('consumer_key');
 
 header('Location: ' . benjaminprevotConnexoon::buildUrl(
     'https://accounts.somfy.com/oauth/oauth/v2/auth',
     array(
         'response_type' => 'code',
         'client_id' => $consumer_key,
-        'redirect_uri' => $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['SERVER_NAME'] . '/index.php?v=d&plugin=' . benjaminprevotConnexoon::ID . '&modal=callback',
+        'redirect_uri' => $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['SERVER_NAME'] . '/index.php?v=d&plugin=' . Plugin::ID . '&modal=callback',
         'state' => $state,
         'grant_type' => 'authorization_code'
     )
