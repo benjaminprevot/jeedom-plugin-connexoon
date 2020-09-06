1f3a02895a7d99288981445b77bcea9552360d00
Benjamin PREVOT <me@benjaminprevot.fr>
2020-09-06 14:33:59 +0200

    [CHANGED] Use utility classes for URL build

--- desktop/modal/authorization.php
+++ desktop/modal/authorization.php
@@ -12,14 +12,14 @@ Config::set('consumer_state', $state);
 
 $consumer_key = Config::get('consumer_key');
 
-header('Location: ' . benjaminprevotConnexoon::buildUrl(
-    'https://accounts.somfy.com/oauth/oauth/v2/auth',
-    array(
-        'response_type' => 'code',
-        'client_id' => $consumer_key,
-        'redirect_uri' => $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['SERVER_NAME'] . '/index.php?v=d&plugin=' . Plugin::ID . '&modal=callback',
-        'state' => $state,
-        'grant_type' => 'authorization_code'
-    )
-));
+$location = HttpRequest::get()
+    ->url('https://accounts.somfy.com/oauth/oauth/v2/auth')
+    ->param('response_type', 'code')
+    ->param('client_id', $consumer_key)
+    ->param('redirect_uri', $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['SERVER_NAME'] . '/index.php?v=d&plugin=' . Plugin::ID . '&modal=callback')
+    ->param('state', $state)
+    ->param('grant_type', 'authorization_code')
+    ->buildUrl();
+
+header('Location: ' . $location);
 ?>
