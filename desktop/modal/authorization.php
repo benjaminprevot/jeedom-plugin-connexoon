47a2c80e821bba904d7059de8f0b644a2b44eaa4
Benjamin PREVOT <me@benjaminprevot.fr>
2020-09-10 07:25:36 +0200

    [MODIFIED] Add config accessors

--- desktop/modal/authorization.php
+++ desktop/modal/authorization.php
@@ -4,17 +4,15 @@ if (!isConnect('admin')) {
 }
 
 Config::unsetAccessToken();
-Config::unset('refresh_token');
-Config::unset('token_exists');
+Config::unsetRefreshToken();
+Config::unsetTokenExists();
 
 $state = hash("sha256", rand());
-Config::set('consumer_state', $state);
-
-$consumer_key = Config::get('consumer_key');
+Config::setConsumerState($state);
 
 $location = HttpRequest::get('https://accounts.somfy.com/oauth/oauth/v2/auth')
     ->param('response_type', 'code')
-    ->param('client_id', $consumer_key)
+    ->param('client_id', Config::getConsumerKey())
     ->param('redirect_uri', $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['SERVER_NAME'] . '/index.php?v=d&plugin=' . Plugin::ID . '&modal=callback')
     ->param('state', $state)
     ->param('grant_type', 'authorization_code')
