933f369c4278d2fb8765d6aca12ff94d03d34913
Benjamin PREVOT <me@benjaminprevot.fr>
2020-09-29 16:40:58 +0200

    [CHANGED] Use flexbox to display widget

--- core/template/dashboard/eqLogic.js
+++ core/template/dashboard/eqLogic.js
@@ -1,9 +1,9 @@
 (function() {
-    'use strict';
+  'use strict';
 
-    $(document).on('click', '.connexoon-eqLogic .cmd', function() {
-        jeedom.cmd.execute({
-            id: $(this).data('cmd_id')
-        });
+  $(document).on('click', '.connexoon .connexoon__commands .commands__command', function() {
+    jeedom.cmd.execute({
+      id: $(this).data('cmd_id')
     });
+  });
 })(jQuery);
--- core/template/mobile/eqLogic.js
+++ core/template/mobile/eqLogic.js
@@ -1,9 +1,10 @@
 (function() {
     'use strict';
-
-    $(document).on('click', '.connexoon-eqLogic .cmd', function() {
-        jeedom.cmd.execute({
-            id: $(this).data('cmd_id')
-        });
+  
+    $(document).on('click', '.connexoon .connexoon__commands .commands__command', function() {
+      jeedom.cmd.execute({
+        id: $(this).data('cmd_id')
+      });
     });
-})(jQuery);
+  })(jQuery);
+  
