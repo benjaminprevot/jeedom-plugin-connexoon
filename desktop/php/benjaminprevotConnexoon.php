87a65376efd9ea935443000d92445248d0fcfd9e
Benjamin PREVOT <me@benjaminprevot.fr>
2020-10-05 20:53:17 +0200

    [ADDED] Manage command name

--- desktop/php/benjaminprevotConnexoon.php
+++ desktop/php/benjaminprevotConnexoon.php
@@ -25,6 +25,11 @@ $eqLogics = eqLogic::byType($plugin->getId());
         </div>
     </div>
     <div class="col-xs-12 eqLogic" style="display: none;">
+        <div class="input-group pull-right" style="display:inline-flex">
+            <span class="input-group-btn">
+                <a class="btn btn-sm btn-success eqLogicAction" data-action="save"><i class="fas fa-check-circle"></i> {{Sauvegarder}}</a>
+			</span>
+		</div>
         <ul class="nav nav-tabs" role="tablist">
             <li role="presentation">
                 <a href="" class="eqLogicAction" aria-controls="home" role="tab" data-toggle="tab" data-action="returnToThumbnailDisplay">
