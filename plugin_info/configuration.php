37282bb5e9fd756f0ac3f7a763cc28db489be49f
Benjamin PREVOT <me@benjaminprevot.fr>
2020-10-06 22:23:16 +0200

    [MODIFIED] Set Callback URL field as editable

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -14,7 +14,7 @@ if (!isConnect()) {
         <div class="form-group">
             <label class="col-sm-2 control-label">{{Callback URL}}</label>
             <div class="col-sm-10">
-                <input type="text" class="form-control" id="configuration--callback_url" readonly />
+                <input type="text" class="form-control" id="configuration--callback_url" />
             </div>
         </div>
         <div class="form-group">
