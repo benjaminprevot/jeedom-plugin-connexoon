059fc60f76bba5c856f46a8f709e41df4f38d57b
Benjamin PREVOT <me@benjaminprevot.fr>
2021-03-09 20:38:41 +0100

    [Added] Current configuration information

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -15,7 +15,7 @@ function fieldset($configuration, $host, $callbackUrl)
 {
     return "
 <fieldset class=\"panel configuration\" data-configuration=\"${configuration}\" data-configuration_callback_url=\"${callbackUrl}\">
-    <legend>${host}</legend>
+    <legend>${host}<span> (Configuration courante)</span></legend>
     <div class=\"form-group\">
         <label class=\"col-sm-2 control-label\">{{Callback URL}}</label>
         <div class=\"col-sm-10\">
@@ -174,6 +174,15 @@ function fieldset($configuration, $host, $callbackUrl)
         display: inline;
         padding: 0 var(--lineheight);
         width: auto;
+    }
+
+    .connexoon__configurations .configuration legend span {
+        display: none;
+        font-style: italic;
+    }
+
+    .connexoon__configurations .configuration.configuration--current legend span {
+        display: inline;
     }
 
     .connexoon__configurations .configuration legend::first-letter {
