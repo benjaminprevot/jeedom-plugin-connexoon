11c777fba055897bae2e7c19a7f2afb4caca356f
Benjamin PREVOT <github@benjaminprevot.fr>
2023-03-18 21:41:28 +0100

    build: Manage code indentation

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -8,9 +8,9 @@ if (!isConnect('admin')) {
 <form class="form-horizontal">
     <fieldset>
         <?php
-		if (version_compare(PHP_VERSION, '7.0') < 0) {
-			echo '<div class="alert alert-danger">{{Attention votre version de PHP (' . PHP_VERSION . ') est trop veille, il faut au minimum PHP 7.0.}}</div>';
-		}
-		?>
+        if (version_compare(PHP_VERSION, '7.0') < 0) {
+            echo '<div class="alert alert-danger">{{Attention votre version de PHP (' . PHP_VERSION . ') est trop veille, il faut au minimum PHP 7.0.}}</div>';
+        }
+        ?>
     </fieldset>
 </form>
