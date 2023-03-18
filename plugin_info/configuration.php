19918160dc9a554c81999c2058ce314156eacf1a
Benjamin PREVOT <github@benjaminprevot.fr>
2023-03-18 21:32:37 +0100

    feat: Manage configuration access

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -1,11 +1,8 @@
 <?php
 require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
-
 include_file('core', 'authentification', 'php');
-
-if (!isConnect()) {
-    include_file('desktop', '404', 'php');
-    die();
+if (!isConnect('admin')) {
+	throw new Exception('{{401 - Accès non autorisé}}');
 }
 ?>
 <div class="alert alert-danger" role="alert">Le plugin est en cours de réécriture suite aux changements imposés par Somfy.</div>
