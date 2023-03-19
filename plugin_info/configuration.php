0dec4308c21d2708e171dc2c1ac059f8e5486646
Benjamin PREVOT <github@benjaminprevot.fr>
2023-03-19 21:15:21 +0100

    feat: Initialize configuration page

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -2,15 +2,47 @@
 require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
 include_file('core', 'authentification', 'php');
 if (!isConnect('admin')) {
-	throw new Exception('{{401 - Accès non autorisé}}');
+    throw new Exception('{{401 - Accès non autorisé}}');
 }
+
+if (version_compare(PHP_VERSION, '7.0') < 0) {
+    echo '<div class="alert alert-danger">{{Attention votre version de PHP (' . PHP_VERSION . ') est trop veille, il faut au minimum PHP 7.0.}}</div>';
+}
+
+$token = config::byKey('somfy::token', 'benjaminprevotConnexoon');
 ?>
-<form class="form-horizontal">
-    <fieldset>
-        <?php
-        if (version_compare(PHP_VERSION, '7.0') < 0) {
-            echo '<div class="alert alert-danger">{{Attention votre version de PHP (' . PHP_VERSION . ') est trop veille, il faut au minimum PHP 7.0.}}</div>';
-        }
-        ?>
-    </fieldset>
-</form>
+<?php if (empty($token)): ?>
+    <form class="form-horizontal">
+        <fieldset>
+            <div class="form-group">
+                <label class="col-sm-3 control-label">{{E-mail}}</label>
+                <div class="col-sm-7">
+                    <input class="form-control">
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-sm-3 control-label">{{Mot de passe}}</label>
+                <div class="col-sm-7">
+                    <input type="password" class="form-control">
+                </div>
+            </div>
+        </fieldset>
+    </form>
+<?php else: ?>
+    <form class="form-horizontal">
+        <fieldset>
+            <div class="form-group">
+                <label class="col-sm-3 control-label">{{Statut}}</label>
+                <div class="col-sm-7">
+                    <span class="label label-success">{{Configuration terminée}}</span>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-sm-3 control-label">{{Action}}</label>
+                <div class="col-sm-7">
+                    <button type="button" class="btn btn-danger btn-sm"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span> {{Supprimer le token}}</button>
+                </div>
+            </div>
+        </fieldset>
+    </form>
+<?php endif; ?>
