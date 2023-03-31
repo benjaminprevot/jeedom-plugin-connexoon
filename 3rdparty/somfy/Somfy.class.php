24fe8c18a66faf4b8610e9b4ef63aa80de3b569f
Benjamin PREVOT <github@benjaminprevot.fr>
2023-03-31 20:27:57 +0200

    feat: Manage configuration

--- 3rdparty/somfy/Somfy.class.php
+++ 3rdparty/somfy/Somfy.class.php
@@ -31,7 +31,49 @@ class Somfy {
         $logMessage = sprintf($errorFormat, $pin, $ip, $httpCode, $response, $error);
         $errorMessage = sprintf(__($errorFormat, __FILE__), $pin, $ip, $httpCode, $response, $error);
 
-        log::add(__CLASS__, 'error', $logMessage);
+        log::add('benjaminprevotConnexoon', 'error', $logMessage);
+
+        throw new Exception($errorMessage);
+    }
+
+    public static function devices($pin, $ip, $token) {
+        $ch = curl_init("https://$pin.local:8443/enduser-mobile-web/1/enduserAPI/setup/devices");
+        curl_setopt($ch, CURLOPT_CAINFO, __DIR__ . '/../../3rdparty/somfy/overkiz-root-ca-2048.crt');
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array("Authorization: Bearer $token"));
+        curl_setopt($ch, CURLOPT_RESOLVE, array("$pin.local:8443:$ip"));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+
+        $response = curl_exec($ch);
+        $error = curl_error($ch);
+
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+        curl_close($ch);
+
+        log::add('benjaminprevotConnexoon', 'error', $httpCode);
+
+        if ($httpCode == 200) {
+            $devices = [];
+
+            foreach (json_decode($response, true) as $device) {
+                $devices[] = array(
+                    'deviceURL' => $device['deviceURL'],
+                    'name'      => $device['label']
+                );
+            }
+
+            return $devices;
+
+            return array_map('Somfy::mapDevices', json_decode($response, true));
+        }
+
+        $errorFormat = 'Impossible de charger les objets pour la gateway %s : IP = %s - HTTP code = %s - Response = %s - Error = %s';
+
+        $logMessage = sprintf($errorFormat, $pin, $ip, $httpCode, $response, $error);
+        $errorMessage = sprintf(__($errorFormat, __FILE__), $pin, $ip, $httpCode, $response, $error);
+
+        log::add('benjaminprevotConnexoon', 'error', $logMessage);
 
         throw new Exception($errorMessage);
     }
--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -5,6 +5,8 @@ if (!isConnect('admin')) {
     throw new Exception('{{401 - Accès non autorisé}}');
 }
 
+require_once dirname(__FILE__) . '/../3rdparty/somfy/Somfy.class.php';
+
 if (version_compare(PHP_VERSION, '7.0') < 0) {
     echo '<div class="alert alert-danger">{{Attention votre version de PHP (' . PHP_VERSION . ') est trop veille, il faut au minimum PHP 7.0.}}</div>';
 }
