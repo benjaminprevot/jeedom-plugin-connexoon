a02ae3dea36f53987ffc7fff5f041163233874ed
Benjamin PREVOT <github@benjaminprevot.fr>
2023-04-01 21:30:25 +0200

    feat: Load Somfy objects

--- 3rdparty/somfy/Somfy.class.php
+++ 3rdparty/somfy/Somfy.class.php
@@ -1,16 +1,9 @@
 <?php
-require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
-include_file('core', 'authentification', 'php');
-
-if (!isConnect('admin')) {
-    throw new Exception(__('401 - Accès non autorisé', __FILE__));
-}
-
 class Somfy {
 
     public static function apiVersion($pin, $ip) {
         $ch = curl_init("https://$pin.local:8443/enduser-mobile-web/1/enduserAPI/apiVersion");
-        curl_setopt($ch, CURLOPT_CAINFO, __DIR__ . '/../../3rdparty/somfy/overkiz-root-ca-2048.crt');
+        curl_setopt($ch, CURLOPT_CAINFO, __DIR__ . '/overkiz-root-ca-2048.crt');
         curl_setopt($ch, CURLOPT_RESOLVE, array("$pin.local:8443:$ip"));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
@@ -26,19 +19,16 @@ class Somfy {
             return json_decode($response, true)['protocolVersion'];
         }
 
-        $errorFormat = 'Impossible de charger les données pour la gateway %s : IP = %s - HTTP code = %s - Response = %s - Error = %s';
+        $errorFormat = 'Impossible de charger la version pour la gateway %s : IP = %s - HTTP code = %s - Response = %s - Error = %s';
 
-        $logMessage = sprintf($errorFormat, $pin, $ip, $httpCode, $response, $error);
-        $errorMessage = sprintf(__($errorFormat, __FILE__), $pin, $ip, $httpCode, $response, $error);
-
-        log::add('benjaminprevotConnexoon', 'error', $logMessage);
+        $errorMessage = sprintf($errorFormat, $pin, $ip, $httpCode, $response, $error);
 
         throw new Exception($errorMessage);
     }
 
     public static function devices($pin, $ip, $token) {
         $ch = curl_init("https://$pin.local:8443/enduser-mobile-web/1/enduserAPI/setup/devices");
-        curl_setopt($ch, CURLOPT_CAINFO, __DIR__ . '/../../3rdparty/somfy/overkiz-root-ca-2048.crt');
+        curl_setopt($ch, CURLOPT_CAINFO, __DIR__ . '/overkiz-root-ca-2048.crt');
         curl_setopt($ch, CURLOPT_HTTPHEADER, array("Authorization: Bearer $token"));
         curl_setopt($ch, CURLOPT_RESOLVE, array("$pin.local:8443:$ip"));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -50,8 +40,6 @@ class Somfy {
         $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
         curl_close($ch);
-
-        log::add('benjaminprevotConnexoon', 'error', $httpCode);
 
         if ($httpCode == 200) {
             $devices = [];
@@ -70,10 +58,7 @@ class Somfy {
 
         $errorFormat = 'Impossible de charger les objets pour la gateway %s : IP = %s - HTTP code = %s - Response = %s - Error = %s';
 
-        $logMessage = sprintf($errorFormat, $pin, $ip, $httpCode, $response, $error);
-        $errorMessage = sprintf(__($errorFormat, __FILE__), $pin, $ip, $httpCode, $response, $error);
-
-        log::add('benjaminprevotConnexoon', 'error', $logMessage);
+        $errorMessage = sprintf($errorFormat, $pin, $ip, $httpCode, $response, $error);
 
         throw new Exception($errorMessage);
     }
